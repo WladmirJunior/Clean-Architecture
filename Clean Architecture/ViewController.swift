8b74090330b79ecb7d30c04f665e54dcb6e78a3d
Wladmir Silva <wladmir.silva@nubank.com.br>
2021-11-11 11:39:26 -0300

    Initial Commit

---
+++ Clean Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean Architecture
+//
+//  Created by Wladmir Silva on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
